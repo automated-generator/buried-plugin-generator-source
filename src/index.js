962b999d30d778e1e687e74e421fdd487d6e8212
吴康俊 <wukangjun@innotechx.com>
2020-02-13 11:27:57 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import path from 'path';
+
+import loaderUtils from 'loader-utils';
+import validateOptions from 'schema-utils';
+
+export default function buriedLoader(content) {
+  return content;
+}
